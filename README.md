87e7cfe3264120b4be1e3ec02374a869829b146a
ADOBENET\ugupta <uditgupta018@gmail.com>
2017-04-15 15:58:09 +0530

    adding comments on validate controller

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ To develop Single Page Travel Blog Application using Angular JS
 npm install -g json-server
 
 json-server --watch db.json --static . --port 9000
+
+git commands to push the code to Github
+1) git add .
+2) git commit -m ""
+3) git push
--- app/controller/validate.controller.js
+++ app/controller/validate.controller.js
@@ -1,5 +1,14 @@
+/* @namespace controller
+ * @desc Validate controller module - to validate the content of sign up and sign in page.
+ * to write logic to display of angular error messages.
+ * if validated succesfully, use user information and create database of record of user
+ *
+ *
+ *
+ */
 (function(){
-
+	
+	
 	angular
 		.module("controller_module",[])
 		.controller("validateCtrl",validateCtrl);
@@ -14,8 +23,26 @@
 		$scope.isPasswordMatched = true;
  		
  		$scope.user = {};
- 		
- 		function validateForm(){
+ 			
+ 		/* this is binding member of controller. When user submits signupForm, it passes control
+ 		 *  to this function
+ 		 */
+ 		$scope.submitForm = function (form){
+			$scope.submitted = true;
+			console.log(form.name);
+
+			validateForm(form);
+			
+		}
+
+		/* @desc : this function is to validate all the fields of bootstrap Sign up page form 
+		 *         /pages/templates/sign-up.html
+		 * @param form : it is bootstrap form variabler "<form name="signUpForm">, this is to 
+		 *         access all form variables.
+		 * @returns : this returns true value if all fields are validated succesfully 
+		 * @memberof : $scope.submitForm (parent function)      
+		 */
+		function validateForm(form){
  			if(form.name.$untouched || form.name.$invalid){
 				$scope.isUsernameValid = false;
 			} else{
@@ -52,12 +79,7 @@
 			}
 
  		};
-
-		$scope.submitForm = function(form){
-			$scope.submitted = true;
-			console.log(form.name)
-			
-		}
+		
 
 	}
 
